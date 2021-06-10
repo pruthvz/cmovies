908b0e0c44cd116f5ee117a99a42a9c69bb2ef16
pruthvi10 <pruthvimohanlal10@gmail.com>
2021-06-10 14:07:15 +0100

    Updates

---
+++ precache-manifest.aaef54ecadde16e68415652778bfbbff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5fbff2c76b067ed1f889ec9c2d38d5",
+    "url": "/cmovies/index.html"
+  },
+  {
+    "revision": "161ccbcc19dccf95a122",
+    "url": "/cmovies/static/css/main.69d1d695.chunk.css"
+  },
+  {
+    "revision": "a725832cf5af5622f2fe",
+    "url": "/cmovies/static/js/2.165166a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cmovies/static/js/2.165166a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161ccbcc19dccf95a122",
+    "url": "/cmovies/static/js/main.49e22d1f.chunk.js"
+  },
+  {
+    "revision": "28de228158b6dd477098",
+    "url": "/cmovies/static/js/runtime-main.61e3fc12.js"
+  }
+]);
